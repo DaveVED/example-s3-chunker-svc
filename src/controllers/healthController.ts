bf88b19e41e9d9667bf2227cc37529eafb9c4c67
DaveVED <dave.denns@gs.com>
2023-11-19 01:00:24 -0600

    :whale: added docker file

--- src/controllers/healthController.ts
+++ src/controllers/healthController.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import { Request, Response, NextFunction } from "express";
 import { ApiResponse } from "../types/Response";
 import { status } from "../services/healthService";
+import { logger } from "../util/logger";
 
 export const healthCheck = (
   req: Request,
@@ -14,6 +15,8 @@ export const healthCheck = (
     const response: ApiResponse = {
       data: healthStatus,
     };
+
+    logger.info(`Health Check Passed ${JSON.stringify(healthStatus)}`);
 
     res.status(StatusCodes.OK).json(response);
   } catch (error) {
