4a55f6fe876f1f9747b636ece27699064e05ca29
DaveVED <dave.denns@gs.com>
2023-11-19 00:27:02 -0600

    :sparkles: basic logger added

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app";
+import { logger } from "./util/logger";
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("HELLO");
+  logger.info(`Chunker service is running on port ${port}`);
 });
